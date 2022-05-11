c28a8cd662fe6aa8077cbb98a443aeefc6b75784
Vustey <vutv579@gmail.com>
2022-05-11 22:25:50 +0700

    thao huong

--- Furuma_Official/main.cpp
+++ Furuma_Official/main.cpp
@@ -3,8 +3,8 @@
 #include "model/Room.h"
 #include "controller/FurumaController.h"
 int main() {
-    Facility* facility=new Room("12","VIP",21,43,9,"Cao","No");
-    facility->output();
+//    Facility* facility=new Room("12","VIP",21,43,9,"Cao","No");
+//    facility->output();
 FurumaController::displayFuruma();
     return 0;
 }
